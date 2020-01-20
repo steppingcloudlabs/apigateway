4d9cbe16a10acec839df57233dce283b379802c9
Prakritidev Verma <prakritidevverma@gmail.com>
2020-01-20 14:58:23 +0530

    : project init

---
+++ src/main/java/com/sclabs/apigateway/ApigatewayApplication.java
@@ -0,0 +1,38 @@
+package com.sclabs.apigateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.sclabs.apigateway.errorfilter.ErrorFilter;
+import com.sclabs.apigateway.postfilter.PostFilter;
+import com.sclabs.apigateway.prefilter.PreFilter;
+import com.sclabs.apigateway.routefilter.RouteFilter;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+}
