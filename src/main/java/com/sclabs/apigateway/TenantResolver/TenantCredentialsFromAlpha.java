e90cad0f6a20559eae965e804c8d7ce67cd8bfe8
Prakritidev Verma <prakritidevverma@gmail.com>
2020-02-05 16:01:04 +0530

    - added comments to the functions for tenant resolver zuul filter.

--- src/main/java/com/sclabs/apigateway/TenantResolver/TenantCredentialsFromAlpha.java
+++ src/main/java/com/sclabs/apigateway/TenantResolver/TenantCredentialsFromAlpha.java
@@ -19,14 +19,12 @@ import com.sclabs.apigateway.TenantResolver.model.ServiceGodsModel;
 import com.sclabs.apigateway.TenantResolver.repository.MasterModelRepository;
 import com.sclabs.apigateway.TenantResolver.service.MasterService;
 
+/*
+ * This Class makes connection with Redis and Mongodb of Alpha's mongodb database.
+ * 
+ */
 @Service
 public class TenantCredentialsFromAlpha {
-
-	/*
-	 * This Class makes connection with Redis and Mongodb of Alpha's database of
-	 * SteppingCloud.
-	 * 
-	 */
 
 	@Autowired
 	MasterModelRepository masterModelRepository;
@@ -34,32 +32,56 @@ public class TenantCredentialsFromAlpha {
 	@Autowired
 	MasterService myMasterService;
 
+	/**
+	 * This function expects company name as a string and returns the document with
+	 * all SF credentials.
+	 * 
+	 * @param Companyname
+	 * @return tenant
+	 */
 	public List<MasterModel> getTenant(String Companyname) {
-
-		final List<MasterModel> duck = myMasterService.getTenant(Companyname);
-		return duck;
+		/**
+		 * 
+		 */
+		final List<MasterModel> tenant = myMasterService.getTenant(Companyname);
+		return tenant;
 	};
-	public List<ServiceGodsModel> getAllService(){
+
+	/**
+	 * This function creates a map that stores all the objectIds of ServicesGods for
+	 * all tenants. If values is not in map, fetch it from redis. If its not in
+	 * redis then get it from mongodb and updated redis as well.
+	 * 
+	 * If value is in map then pass.
+	 * 
+	 * @return _id
+	 */
+	public List<ServiceGodsModel> getService() {
 		/**
-		 * 1. Create a map that will store all the objectIds of ServicesGods for all tenants.
-		 * 2. If values is not in map? fetch it from redis. If its not in redis then get it from mongodb and updated redis as well. 
-		 * 3. If value is in map -> use it. 
+		 *
 		 */
-		
+
 		return null;
-		
+
 	}
-	public List<ServiceGodsModel> getServiceCredentials(String name, ObjectId mongoId){
-		
+
+	/**
+	 * This function fetch the credentials of serviceGods.
+	 * @param tenantid
+	 * @param serviceid
+	 * @return document
+	 */
+	public List<ServiceGodsModel> getServiceCredentials(String tenantid, ObjectId serviceid) {
+
 		/**
-		 * 1. If serviceName exists? Then lookup in map created by  getAllService(). 
-		 * 2. If mongoId exists in map. Then get the data of that service from from redis.
-		 * 3. If not in redis, then get it from mongodb and updated redis.
-		 * 4. return credentials. 
+		 * 1. If serviceName exists? Then lookup in map created by getAllService(). 2.
+		 * If mongoId exists in map. Then get the data of that service from from redis.
+		 * 3. If not in redis, then get it from mongodb and updated redis. 4. return
+		 * credentials.
 		 */
-		
+
 		return null;
-		
+
 	}
 
 }
