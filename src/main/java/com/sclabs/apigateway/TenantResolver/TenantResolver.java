4aa469525dc8e6fd9474c5ef9f88a216df49f6cd
Prakritidev Verma <prakritidevverma@gmail.com>
2020-02-05 16:44:22 +0530

    -

--- src/main/java/com/sclabs/apigateway/TenantResolver/TenantResolver.java
+++ src/main/java/com/sclabs/apigateway/TenantResolver/TenantResolver.java
@@ -6,12 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -30,10 +24,6 @@ public class TenantResolver extends ZuulFilter {
 
 	@Override
 	public Object run() throws ZuulException {
-		// ObjectMapper Obj = new ObjectMapper();
-		// Obj.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true);
-		// Obj.enable(SerializationFeature.INDENT_OUTPUT);
-		// Gson gson = new Gson();
 
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
@@ -41,22 +31,10 @@ public class TenantResolver extends ZuulFilter {
 		String companyName = request.getHeader("companyname");
 		String serviceName = request.getHeader("servicename");
 
-		List<MasterModel> duck = child.getAllCredentials(companyName);
-
-		System.out.println(duck);
-		// String numbersJson = gson.toJson(duck);
-		// System.out.print(numbersJson);
-
-		// String jsonStr;
-		// try {
-		// jsonStr = Obj.readValue(duck);
-		// System.out.print(jsonStr);
-
-		// } catch (final JsonProcessingException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-
+		List<MasterModel> duck = child.getTenant(companyName);
+		
+		System.out.println(duck.get(0).getService_name());
+		
 		return null;
 	}
 
